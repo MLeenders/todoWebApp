efa1f304e00b1397d0062569a7c5a43ebdbc315a
Administrator <marit.leenders@hotmail.com>
2017-03-27 10:13:07 +0200

    initial commit

---
+++ src/main/java/nl/leenders/todoapp/api/ToDoApi.java
@@ -0,0 +1,146 @@
+package nl.leenders.todoapp.api;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import nl.leenders.todoapp.models.TodoModel;
+
+
+@Path("todo")
+public class ToDoApi {
+	
+	static private ArrayList<TodoModel> todootjes = new ArrayList<TodoModel>();
+	
+	static  { 
+		todootjes.add(new TodoModel(1, "boodschappen", "huishoudelijk", new Date()));
+		todootjes.add(new TodoModel(2, "zitten", "chillen", new Date()));
+		
+	}
+	
+	
+	@GET
+	
+	@Produces(MediaType.APPLICATION_JSON)
+	
+	public ArrayList<TodoModel>  getTodoModel() { 
+		
+		todootjes.add(new TodoModel(1, "boodschappen", "huishoudelijk", new Date()));
+		todootjes.add(new TodoModel(2, "zitten", "chillen", new Date()));
+		
+		return todootjes;
+		
+	}
+	
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public TodoModel getTodoModelById(@PathParam("id") int id){ 
+		for (int i=0; i<todootjes.size(); i++) { 
+			if (todootjes.get(i).getId() == id) { 
+				return todootjes.get(i);
+			}
+		}
+		
+		// if not found -> return empty
+		return new TodoModel();
+	}
+	
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void addNewTodo(TodoModel newTodootje){ 
+		
+		todootjes.add(newTodootje); 
+	}
+	
+	@PUT
+	@Path("/complete/{id}")
+	public void markTodoAsComplete(@PathParam("id") int id){ 
+	// op zoek naar het todootje met id 'id' en markeer als compleet
+		
+		for (int i=0; i<todootjes.size(); i++) { 
+			if (todootjes.get(i).getId() == id) { 
+				todootjes.get(i).setCompleted(true);
+				
+			}
+		
+		} 
+	
+	}
+
+	
+	@DELETE
+	@Path("/{id}")
+	public void deleteTodo(@PathParam("id") int id){ 
+		
+		//op zoek naar id en delete deze
+		for (int i=0; i<todootjes.size(); i++) { 
+			if (todootjes.get(i).getId() == id) { 
+				
+				todootjes.remove(i);
+			}
+		
+	}
+	
+} 
+	
+	@GET
+	@Path("/filtered") 
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<TodoModel> getFilteredTodos(@QueryParam("category") String category) { 
+		
+		
+		
+		ArrayList<TodoModel> filteredTodos = new ArrayList<TodoModel>();
+		
+		for (int i=0; i<todootjes.size(); i++) { 
+			if (todootjes.get(i).getCategory().equals(category) ) { 
+				 filteredTodos.add(todootjes.get(i));
+			
+			}
+		
+	}
+		
+		return filteredTodos;
+	
+		//Andere (en kortere, maar ingewikkeldere) manier van filteren:
+		// return todootjes.stream().filter(todo -> todo.getCategory().
+		//equalsIgnoreCase(category)).collect(Collectors.toList());
+} 
+	
+	@PUT
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void changeTodo(@PathParam("id") int id, TodoModel aanTePassen){ 
+		
+		//op zoek naar id en pas aan
+		for (int i=0; i<todootjes.size(); i++) { 
+			if (todootjes.get(i).getId() == id) { 
+				TodoModel oudModel = todootjes.get(i); 
+				oudModel.setCategory(aanTePassen.getCategory());
+				oudModel.setDescription(aanTePassen.getDescription());
+				oudModel.setCompleted(aanTePassen.isCompleted());
+				oudModel.setDueDate(aanTePassen.getDueDate());
+				
+			}
+		}
+	}	
+}
+	
+
+	
+
+	
+	
+
---
+++ src/main/java/nl/leenders/todoapp/models/TodoModel.java
@@ -0,0 +1,62 @@
+package nl.leenders.todoapp.models;
+
+import java.util.Date;
+
+public class TodoModel {
+
+	
+	private int id;
+	private String description;
+	private String category;
+	private Date dueDate;
+	private boolean completed;
+	
+	//we need an empty constructor
+	public TodoModel() { }
+		
+	public TodoModel(int id, String description, String category, Date dueDate) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.category = category;
+		this.dueDate = dueDate;
+		
+	}
+
+
+	public boolean isCompleted() {
+		return completed;
+	}
+
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public Date getDueDate() {
+		return dueDate;
+	}
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+	
+	
+	
+}
